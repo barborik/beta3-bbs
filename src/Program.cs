2eb6d27c3a5d277b9a79b8df1a96212e5b05b53b
Barbo24 <barborikadam@gmail.com>
2023-01-24 00:44:04 +0100

    backup

---
+++ src/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BBS
+{
+    using Terminal.Gui;
+    using Renci.SshNet;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Application.Init();
+            Application.Run(new BBS.Page.Login());
+            Application.Shutdown();*/
+        }
+    }
+}
